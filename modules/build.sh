a5f2e289318b10d48471a3b938fd95e6f8d13393
Michael Clausen <encodeering@gmail.com>
2018-02-11 15:45:08 +0100

    initial project configuration for hubot 2.19.0
    * npm install -g doesn't work on armhf, so we skip the build for now
    
    NOTE
    
    * e.g. docker build . -f- <<< "FROM encodeering/hubot-amd64:2.19-alpine-onbuild"

---
+++ modules/build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -e
+
+import com.encodeering.ci.config
+import com.encodeering.ci.docker
+
+docker-pull "$REPOSITORY/node-$ARCH:9.3-alpine" "node:9.3-alpine"
+
+docker-build -f "$PROJECT/Dockerfile" --build-arg version="$PIN" "$PROJECT"
