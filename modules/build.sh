d30de774260378d03c4023ebf7ad15121517b057
Michael Clausen <encodeering@gmail.com>
2018-02-18 18:58:27 +0100

    adds a verification step
    * should print the hubot version

--- modules/build.sh
+++ modules/build.sh
@@ -8,3 +8,5 @@ import com.encodeering.ci.docker
 docker-pull "$REPOSITORY/node-$ARCH:9.3-alpine" "node:9.3-alpine"
 
 docker-build -f "$PROJECT/Dockerfile" --build-arg version="$PIN" "$PROJECT"
+
+docker-verify -v
