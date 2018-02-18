e674a0c6bffa9beef1cfd4f444e606066359fb5e
Michael Clausen <encodeering@gmail.com>
2018-02-18 19:19:59 +0100

    combines npm install calls
    * better to have less calls and modules might be empty, which may break on future npm version (e.g. yarn won't handle this)

--- modules/hubot/bin/docker-entrypoint.sh
+++ modules/hubot/bin/docker-entrypoint.sh
@@ -6,7 +6,6 @@ echo "${HUBOT_SCRIPTS} hubot-help hubot-diagnostics" \
    | xargs -i jq -n -c -M --arg s {} '$s|split(" ")' > external-scripts.json
 
 npm install `jq -r ".[]"                               external-scripts.json`
-npm install "hubot-${HUBOT_ADAPTER}"
-npm install       "${HUBOT_MODULES}"
+npm install "hubot-${HUBOT_ADAPTER}" "${HUBOT_MODULES}"
 
 exec npm start -- --adapter "${HUBOT_ADAPTER}" "$@"
