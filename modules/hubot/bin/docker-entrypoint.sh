c471a75ac0756d3aca541c7b8fcf8e3b3f1eba70
Michael Clausen <encodeering@gmail.com>
2018-02-18 17:58:40 +0100

    adds a startup scripts to handle dynamic env
    * scripts, modules and adapter will now be installed from corresponding env vars
    
    NOTE
    
    * exec as shell replacement for pid 0

---
+++ modules/hubot/bin/docker-entrypoint.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo "${HUBOT_SCRIPTS} hubot-help hubot-diagnostics" \
+   | xargs -i jq -n -c -M --arg s {} '$s|split(" ")' > external-scripts.json
+
+npm install `jq -r ".[]"                               external-scripts.json`
+npm install "hubot-${HUBOT_ADAPTER}"
+npm install       "${HUBOT_MODULES}"
+
+exec npm start -- --adapter "${HUBOT_ADAPTER}" "$@"
